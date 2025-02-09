ffa395491d388434da38778e12ecd34388f88b98
vm-havsite <vivaanmehta0489@gmail.com>
2025-02-09 08:52:40 +0530

    created file upload system

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, render_template
+import os
+
+app = Flask(__name__)
+
+# Set the upload folder
+UPLOAD_FOLDER = 'uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+# Create the uploads folder if it doesn't exist
+if not os.path.exists(UPLOAD_FOLDER):
+    os.makedirs(UPLOAD_FOLDER)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return 'No file part'
+    file = request.files['file']
+    if file.filename == '':
+        return 'No selected file'
+    file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+    return 'File uploaded successfully!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
