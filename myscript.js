55b8e14d839f5e16f375ad5652f4b0248c99e605
vm-havsite <vivaanmehta0489@gmail.com>
2025-02-26 18:57:33 +0530

    added day and night mode

---
+++ myscript.js
@@ -0,0 +1,20 @@
+// Apply theme on page load
+document.addEventListener('DOMContentLoaded', () => {
+    const theme = localStorage.getItem('theme');
+    if (theme === 'dark') {
+      document.getElementById('html-root').classList.add('dark-mode');
+    }
+  });
+  
+  // Toggle theme and save preference to localStorage
+  function toggleTheme() {
+    const htmlRoot = document.getElementById('html-root');
+    if (htmlRoot.classList.contains('dark-mode')) {
+      htmlRoot.classList.remove('dark-mode');
+      localStorage.setItem('theme', 'light');
+    } else {
+      htmlRoot.classList.add('dark-mode');
+      localStorage.setItem('theme', 'dark');
+    }
+  }
+  
